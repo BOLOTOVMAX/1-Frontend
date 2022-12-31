c24fd4c0f9e29a80fc5093e3298b069fe6be4566
BOLOTOVMAX <bolotovmax1983@mail.ru>
2022-12-31 09:41:19 +0300

    First Commit

---
+++ scrept.js
@@ -0,0 +1,80 @@
+// Пример №1
+let b1 = 5 % 3;
+let b2 = 3 % 5;
+let b3 = 5 + '3';
+let b4 = '5' - 3;
+let b5 = 75 + 'кг';
+
+console.log(b1)
+console.log(b2)
+console.log(b3)
+console.log(b4)
+console.log(b5)
+
+// Пример №2 
+let heightC = 10;
+let dC = 4;
+let v = dC*heightC;
+
+console.log(v)
+
+
+// Пример №3
+let a = 10;
+let b = 2;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+
+// Пример №4
+let c = 15;
+let d = 2;
+
+console.log( result = c + d);
+
+// Пример №5
+console.log(a + b +(c / 3))
+
+// Пример №6
+a = a + 7;
+b = b + 8;
+c = a + b;
+d = d + 5;
+console.log(result = c + d);
+
+// Пример №7
+let name = 'Максим';
+console.log('Привет, ' + name + ' !')
+
+// Пример №8
+let age = 39;
+console.log('Мне ' + age + ' лет !');
+
+// Пример №9
+let str = 'abcde';
+console.log(str[0]);
+console.log(str[2]);
+console.log(str[4]);
+
+//Пример №10
+console.log(60 * 60);
+console.log(3600 * 24);
+console.log(86400 * 30);
+
+//Пример №11
+ let num = 47;
+ num += 7;
+ num -= 18;
+ num *= 10;
+ num /= 15;
+ console.log(num);
+
+ // Пример №12
+ num = 10;
+ num ++;
+ num ++;
+ num --;
+ console.log(num);
